9e617ffc28c7938ab85dc10d306e8b96c52f6ca8
minniek <minniek@bu.edu>
2015-04-12 14:39:17 -0400

    removed unused file

--- server/server_v0.php
+++
@@ -1,8 +0,0 @@
-<?php	
-	$finalArray = array();
-	foreach (getallheaders() as $headerName => $headerValue) {
-		$myArray = array($headerName => $headerValue);
-		$finalArray = array_merge($finalArray, $myArray);
-	}
-	echo json_encode($finalArray);
-?>
