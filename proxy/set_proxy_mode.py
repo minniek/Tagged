37c3d652b9e3e3da81e29157b8d63d8e91bdc487
minniek <minniek@bu.edu>
2015-04-25 16:23:39 -0400

    Update set_proxy_mode.py

--- proxy/set_proxy_mode.py
+++ proxy/set_proxy_mode.py
@@ -1,5 +1,5 @@
 '''
-Set Tagged Proxy modes using GPIO pins
+Set Tagged Proxy modes using Raspberry Pi 2 GPIO pins
 
 Usage: python3 set_proxy_mode.py
 
@@ -13,9 +13,8 @@ import os, time
 GPIO.setmode(GPIO.BOARD) 
 
 '''
-Set up button and LED pins for Tagged Proxy mode one: 'a' or 'v'
-If mode one is v: add X-Tagged header from client-->server
-If mode one is a: do nothing
+Set up button and LED pins for Tagged Proxy mode "v"
+If mode one is v: inject X-Tagged header from client-->server
 '''
 btn_1 = 11
 GPIO.setup(btn_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -24,7 +23,7 @@ GPIO.setup(led_1, GPIO.OUT)
 GPIO.output(led_1, 0)
 
 '''
-Set up button and LED pins for Tagged Proxy mode two 'x'
+Set up button and LED pins for Tagged Proxy mode "x"
 If mode is x: remove X-Tagged header from server-->client to invalidate server's digital signature
 Mode x requires the X-Tagged header to be injected. Therefore, write "vx" to proxy_config file
 '''
@@ -34,26 +33,39 @@ led_2 = 21
 GPIO.setup(led_2, GPIO.OUT)
 GPIO.output(led_2, 0)
 
-# TODO Replace button with push button or switch, add debouncing if needed
+'''
+Set up button and LED pins for Tagged Proxy default mode "a"
+If mode is a: no header injection
+'''
+btn_3 = 29
+GPIO.setup(btn_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+led_3 = 31
+GPIO.setup(led_3, GPIO.OUT)
+GPIO.output(led_3, 0)
+
 try:
-	os.system("echo -n a > proxy_config") # Set default mode
+	os.system("echo a > proxy_config") # Set default mode
 	print("Waiting for input...")
 	while True:
 		if (GPIO.input(btn_1)):
-			print("Tagged Proxy mode one: v")
-			os.system("echo -n v > proxy_config")
+			print("Tagged Proxy in mode v")
+			os.system("echo v > proxy_config")
 			GPIO.output(led_1, 1)
 		else:
 			GPIO.output(led_1, 0)
 
 		if (GPIO.input(btn_2)):
-			print("Tagged Proxy mode two: x")
-			os.system("echo -n vx > proxy_config")
+			print("Tagged Proxy mode x")
+			os.system("echo vx > proxy_config")
 			GPIO.output(led_2, 1)
 		else:
 			GPIO.output(led_2, 0)
-
-		#if not (GPIO.input(btn_1)) and (GPIO.input(btn_2)):
-			#os.system("echo -n a > proxy_config")
+			
+		if (GPIO.input(btn_3)):
+                        print("Tagged Proxy in mode a")
+                        os.system("echo a > proxy_config")
+                        GPIO.output(led_3, 1)
+		else:
+			GPIO.output(led_3, 0)
 except KeyboardInterrupt:
 	GPIO.cleanup() # Clean up all GPIO pins for clean exit
