d3df2cdcc386b9f7dcbfa2f669ffaf279bd56d28
minniek <minniek@bu.edu>
2015-05-09 13:41:48 -0400

    updated comments

--- generate_rsa_keys.py
+++ generate_rsa_keys.py
@@ -2,8 +2,8 @@
 
 import os
 
-# Generate private key to generate digital signature 
+# Create private key (used to generate digital signature in tagged_server.php)
 os.system("openssl genrsa -out private_key.pem 2048")
 
-# Generate public key from private (to be used with Tagged Android app)
+# Generate public key from private key (to be used with Tagged Android application to verify Tagged server's digital signature)
 os.system("openssl rsa -in private_key.pem -pubout -outform PEM -out public_key.pem")
