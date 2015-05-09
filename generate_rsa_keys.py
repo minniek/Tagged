886a99618b0ba6b023dbdd980ef00a80b962e73e
minniek <minniek@bu.edu>
2015-05-09 13:38:04 -0400

    updated comments

--- generate_rsa_keys.py
+++ generate_rsa_keys.py
@@ -1,12 +1,9 @@
-# Generate RSA keys (to be used with Tagged app)
+# Generate RSA keys for Tagged Server
 
 import os
-
-# TODO
-# Give user option to store keys in specified directory
 
 # Generate private key
 os.system("openssl genrsa -out private_key.pem 2048")
 
-# Generate public key from private
+# Generate public key from private (to be used with Tagged Android app)
 os.system("openssl rsa -in private_key.pem -pubout -outform PEM -out public_key.pem")
